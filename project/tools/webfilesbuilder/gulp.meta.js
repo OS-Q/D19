2af4ba38ff3f31a34609c3be09f5e696ef9bab43
qitas <qitas@hotmail.com>
2019-07-17 14:34:19 +0800

    fix index

--- project/tools/webfilesbuilder/gulp.meta.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function () {
-  return {
-    packages: [ 'gulp-concat', 'gulp-htmlmin', 'gulp-flatmap', 'gulp-gzip', 'gulp-uglify', 'gulp-clean-css', 'fs', 'path', 'pump'],
-    deployFiles: [ 'gulpfile.js', 'gulpcss.js'],
-    take: 'last-line'
-  };
-};
